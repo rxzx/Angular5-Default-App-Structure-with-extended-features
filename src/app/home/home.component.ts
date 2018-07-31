2fdcae058f8ab025eb5e0d019a8a3e35c95af630
razasultan <rxzx.sultxn@gmail.com>
2018-07-31 16:27:39 +0500

    up

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -30,10 +30,8 @@ export class HomeComponent implements OnInit {
       });
 
     this.cols = [
-      { field: 'vin', header: 'Vin' },
-      { field: 'year', header: 'Year' },
-      { field: 'brand', header: 'Brand' },
-      { field: 'color', header: 'Color' }
+      { field: 'name', header: 'Name' },
+      { field: 'province', header: 'Province' }
     ];
 
     this.loading = true;
@@ -41,23 +39,6 @@ export class HomeComponent implements OnInit {
   }
 
 
-  loadCarsLazy(event: LazyLoadEvent) {
-    this.loading = true;
 
-    //in a real application, make a remote request to load data using state metadata from event
-    //event.first = First row offset
-    //event.rows = Number of rows per page
-    //event.sortField = Field name to sort with
-    //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-    //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
-    //imitate db connection over a network
-    setTimeout(() => {
-      if (this.data) {
-        this.cities = this.data.slice(event.first, (event.first + event.rows));
-        this.loading = false;
-      }
-    }, 1000);
-  }
 
 }
